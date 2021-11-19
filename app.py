2a341b2471f3e0dd9fd523035812fb0532312802
ramon-diaz <ramndiaz@gmail.com>
2021-11-19 09:42:53 -0400

    second commit

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from products import products
 @app.route('/ping', methods=['Get'])
 def ping():
     return jsonify({'message': 'pong'})
+
 @app.route('/products', methods=['Get'])
 def getProducts():
     return jsonify({'products': products, 'message': 'products list'})
+
 @app.route('/products/<string:product_name>', methods=['Get'])
 def getProduct(product_name):
     productsFound = [product for product in products if product['name'] == product_name]
@@ -17,6 +19,7 @@ def getProduct(product_name):
         return jsonify({'product': productsFound[0]})
     else:
         return jsonify({'message': 'product not found'})
+
 @app.route('/products', methods=['POST'])
 def addProducts():
     newProduct= {
@@ -26,6 +29,7 @@ def addProducts():
     }
     products.append(newProduct)
     return jsonify({'message': 'product added', 'products': products})
+
 @app.route('/products/<string:product_name>', methods=['PUT'])
 def editProduct(product_name):
     productFound = [product for product in products if product['name'] == product_name ]
@@ -39,6 +43,7 @@ def editProduct(product_name):
         })
     else:
         return jsonify({'message': 'product not found'})
+
 @app.route('/products/<string:product_name>', methods=['DELETE'])
 def deleteProduct(product_name):
     productFound = [product for product in products if product['name'] == product_name ]
@@ -50,6 +55,23 @@ def deleteProduct(product_name):
         })
     else:
         return jsonify({'message': 'product not found'})
+
+@app.route('/fizzbuzz/<int:last_num>', methods=['Get'])
+def fizzbuzz(last_num):
+    for x in range(1, last_num):
+        if x % 15 == 0:
+            print("%s FizzBuzz" %x)
+            continue
+        elif x % 3 == 0:
+            print("%sFizz" %x)
+            continue
+        elif x % 5 == 0:
+            print("%s Buzz" %x)
+            continue
+        else:
+            print(x)
+            continue
+    return jsonify({'message': "print in console"})
 
 if __name__ == '__main__':
     app.run(debug = True, port = 4000)
